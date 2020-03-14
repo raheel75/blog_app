af6891af6d35d57a153871410363c3aa69ecd23d
raheel <raheel.xovolt@gmail.com>
2020-03-14 12:06:53 +0500

    forms and accounts

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+
+        self.post = Post.objects.create(
+            title='a good title',
+            body='content body',
+            author=self.user
+        )
